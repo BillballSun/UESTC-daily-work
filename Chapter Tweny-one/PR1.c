7bcb7cef4a291b41e3a8f1e63285a29028634ecc
Bill Sun <captainallredbillball@gmail.com>
2017-03-13 19:57:34 +0800

    Add files via upload

---
+++ Chapter Tweny-one/PR1.c
@@ -0,0 +1,59 @@
+//PR7
+//(a)
+Show time, time.h
+//(b)
+isdigit, ctype.h
+//(c)
+unsigned int, max is UINT_MAX in limit.h
+//(d)
+float round up/down, math.h
+//(e)
+CHAR_BIT limit.h
+//(f)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
